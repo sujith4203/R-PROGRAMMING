7c04994e80ad8de3e01c177a74b48cd986a569f7
B.sujith <110800611+sujith4203@users.noreply.github.com>
2023-01-31 09:47:51 +0530

    Add files via upload

---
+++ iris(dataframe).R
@@ -0,0 +1,9 @@
+length<-c(2.1,3.4,2,5,2,7,2.9)
+diameter<-c(0.3,0.5,0.6,0.9,1.1)
+V<-length*pi*(diameter/2)2
+V
+
+a<-iris
+tapply(iris$Sepal.Length, iris$Species, mean)
+
+tapply(iris$Petal.Length, iris$Species, mean)
